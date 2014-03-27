804a9d3fe34abe1a588e57d5bb6fa0c8069b4026
shuidong <lsd751@qq.com>
2014-03-27 23:09:15 +0800

    for webstorm github testing

---
+++ src/test.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: rasuitou
+ * Date: 14-3-27
+ * Time: 下午11:06
+ * To change this template use File | Settings | File Templates.
+ */
+
+function testU(){
+    var key: 0,
+        var value:1
+
+}
